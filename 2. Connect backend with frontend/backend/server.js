44e5e091f7064313976e270c0e33f21c2fc341c1
Vedantjn <vedant.jain.indore@gmail.com>
2024-08-05 17:41:30 +0530

    first commit

---
+++ 2. Connect backend with frontend/backend/server.js
@@ -0,0 +1,51 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+const app = express();
+
+app.use(cors);
+
+// app.get('/', (req, res) => {
+//     res.send('Server is ready');
+// });
+
+// get a list of 5 jokes
+
+app.get('/api/jokes', (req, res) => {
+    const jokes = [
+        {
+            id: 1,
+            title: "Why don't scientists trust atoms?",
+            content: "Because they make up everything!"
+        },
+        {
+            id: 2,
+            title: "Why did the scarecrow win an award?",
+            content: "Because he was outstanding in his field!"
+        },
+        {
+            id: 3,
+            title: "Why don't skeletons fight each other?",
+            content: "They don't have the guts."
+        },
+        {
+            id: 4,
+            title: "What do you call fake spaghetti?",
+            content: "An impasta!"
+        },
+        {
+            id: 5,
+            title: "Why was the math book sad?",
+            content: "Because it had too many problems."
+        }
+    ];
+    
+    res.json(jokes);
+});
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Serve at http://localhost:${port}`);
+})
